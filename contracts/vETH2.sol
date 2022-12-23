b977aef2308e03e7b7d46219dc41d32df3466739
Edwin Wang <lark930@gmail.com>
2022-12-23 22:55:44 +0800

    Optimize .solhint.json

--- contracts/vETH2.sol
+++ contracts/vETH2.sol
@@ -5,7 +5,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+// solhint-disable-next-line contract-name-camelcase
 contract vETH2 is ERC20Pausable, Ownable {
+    // solhint-disable-next-line no-empty-blocks
     constructor() ERC20("Voucher Ethereum 2.0", "vETH2") Pausable() Ownable() {}
 
     function mint(address account, uint amount) external onlyOwner {
