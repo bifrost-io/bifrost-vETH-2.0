8b9b4106458e7d03f43f7d2575cce60ba49e959d
Edwin Wang <lark930@gmail.com>
2022-12-24 21:31:47 +0800

    Add DepositContract.ts

---
+++ deploy/DepositContract.ts
@@ -0,0 +1,18 @@
+import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+const deployFunction: DeployFunction = async function ({ deployments, getNamedAccounts }: HardhatRuntimeEnvironment) {
+  console.log('Running DepositContract deploy script')
+
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+  const { address } = await deploy('DepositContract', { from: deployer })
+
+  console.log('DepositContract deployed at', address)
+}
+
+export default deployFunction
+
+deployFunction.dependencies = ['']
+
+deployFunction.tags = ['DepositContract']
