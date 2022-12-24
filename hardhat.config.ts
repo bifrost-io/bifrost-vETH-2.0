c66c1d745ed239dccf42e10d50fcb9c6cee08bde
Edwin Wang <lark930@gmail.com>
2022-12-24 21:12:52 +0800

    Add DepositContract.sol

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -35,13 +35,20 @@ const config: HardhatUserConfig = {
     },
   },
   solidity: {
-    version: '0.8.17',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
+    compilers: [
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
       },
-    },
+      {
+        version: '0.6.11',
+      },
+    ],
   },
   contractSizer: {
     alphaSort: true,
