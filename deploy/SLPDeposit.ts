1bda826619d68aea5f14cf78b17725d80aeba76f
Edwin Wang <lark930@gmail.com>
2022-12-26 16:10:02 +0800

    set merkle root while deploy

--- deploy/SLPDeposit.ts
+++ deploy/SLPDeposit.ts
@@ -1,5 +1,6 @@
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { ethers } from 'hardhat'
 
 const deployFunction: DeployFunction = async function ({ deployments, getNamedAccounts }: HardhatRuntimeEnvironment) {
   console.log('Running SLPDeposit deploy script')
@@ -23,6 +24,12 @@ const deployFunction: DeployFunction = async function ({ deployments, getNamedAc
       },
     },
   })
+
+  const slpDeposit = await ethers.getContractAt('SLPDeposit', address)
+  const index = 0
+  const merkleRoot = '0xd68ce67b1e69f61353fd887b267493c2f1401f9711c0c2b1744bbe8cbf27938f'
+  await slpDeposit.setMerkleRoot(index, merkleRoot)
+  console.log('\x1b[32m%s\x1b[0m', `Merkle root at index 0 is ${await slpDeposit.merkleRoots(index)}`)
 
   console.log('SLPDeposit deployed at', address)
 }
