70bfeed8ffd4f95879375615ac3452ab475ab542
Edwin Wang <lark930@gmail.com>
2023-03-01 17:34:09 +0800

    Fix veth2-set-operator.ts

--- tasks/veth2-set-operator.ts
+++ tasks/veth2-set-operator.ts
@@ -5,7 +5,7 @@ task('veth2-set-operator', 'Set operator').setAction(async function (
   { ethers, network, deployments }
 ) {
   const vETH2 = await ethers.getContractAt('vETH2', (await deployments.get('vETH2')).address)
-  const SLPCore = await ethers.getContractAt('SLPCore', (await deployments.get('vETH2')).address)
+  const SLPCore = await ethers.getContractAt('SLPCore', (await deployments.get('SLPCore')).address)
   const tx = await vETH2.setOperator(SLPCore.address)
 
   console.log(`vETH2 operator set: ${tx.hash}`)
