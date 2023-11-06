7e9b511d92382399229d82e0f6735ace030a5577
Edwin Wang <lark930@gmail.com>
2023-11-06 23:47:47 +0800

    Add ISSVClusters to SLPDeposit

--- contracts/SLPDeposit.sol
+++ contracts/SLPDeposit.sol
@@ -26,6 +26,60 @@ interface IETHDepositor {
     function depositETH() external payable;
 }
 
+interface ISSVClusters {
+    /// @notice Represents a cluster of validators
+    struct Cluster {
+        /// @dev The number of validators in the cluster
+        uint32 validatorCount;
+        /// @dev The index of network fees related to this cluster
+        uint64 networkFeeIndex;
+        /// @dev The last index calculated for the cluster
+        uint64 index;
+        /// @dev Flag indicating whether the cluster is active
+        bool active;
+        /// @dev The balance of the cluster
+        uint256 balance;
+    }
+
+    /// @notice Registers a new validator on the SSV Network
+    /// @param publicKey The public key of the new validator
+    /// @param operatorIds Array of IDs of operators managing this validator
+    /// @param sharesData Encrypted shares related to the new validator
+    /// @param amount Amount of SSV tokens to be deposited
+    /// @param cluster Cluster to be used with the new validator
+    function registerValidator(
+        bytes calldata publicKey,
+        uint64[] memory operatorIds,
+        bytes calldata sharesData,
+        uint256 amount,
+        Cluster memory cluster
+    ) external;
+
+    /// @notice Liquidates a cluster
+    /// @param owner The owner of the cluster
+    /// @param operatorIds Array of IDs of operators managing the cluster
+    /// @param cluster Cluster to be liquidated
+    function liquidate(address owner, uint64[] memory operatorIds, Cluster memory cluster) external;
+
+    /// @notice Removes an existing validator from the SSV Network
+    /// @param publicKey The public key of the validator to be removed
+    /// @param operatorIds Array of IDs of operators managing the validator
+    /// @param cluster Cluster associated with the validator
+    function removeValidator(bytes calldata publicKey, uint64[] memory operatorIds, Cluster memory cluster) external;
+
+    /// @notice Reactivates a cluster
+    /// @param operatorIds Array of IDs of operators managing the cluster
+    /// @param amount Amount of SSV tokens to be deposited for reactivation
+    /// @param cluster Cluster to be reactivated
+    function reactivate(uint64[] memory operatorIds, uint256 amount, Cluster memory cluster) external;
+
+    /// @notice Withdraws tokens from a cluster
+    /// @param operatorIds Array of IDs of operators managing the cluster
+    /// @param tokenAmount Amount of SSV tokens to be withdrawn
+    /// @param cluster Cluster where the withdrawal will be made
+    function withdraw(uint64[] memory operatorIds, uint256 tokenAmount, Cluster memory cluster) external;
+}
+
 contract SLPDeposit is OwnableUpgradeable {
     struct Validator {
         bytes pubkey;
